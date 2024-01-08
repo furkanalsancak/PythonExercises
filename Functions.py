4ec7667418b04d794e0dbfe80d25d092f2111780
furkanalsancak <furkanalsancak3084@gmail.com>
2024-01-08 21:48:32 +0000

    new files

--- Functions.py
+++ Functions.py
@@ -72,32 +72,3 @@ add_two = lambda x : x + 2
 print(add_two(5)) 
 '''
 
-
-def digital_root(n):
-    # Continue the loop until n is a single-digit number
-    while n >= 10:
-        # Convert the number to a list of its digits
-        digits = []
-        for digit in str(n):
-            digits.append(int(digit))
-        
-        # Calculate the sum of the digits
-        digit_sum = sum(digits)
-        
-        # Display the process
-        print(f"{n}  -->  {' + '.join(map(str, digits))} = {digit_sum}", end="  -->  ")
-
-        # Update n with the sum of digits
-        n = digit_sum
-
-    print(n)  # Print the final result
-    return n
-
-# Example usage:
-nums = [942, 456]
-
-for num in nums:
-    result = digital_root(num)
-    print(f"The digital root of {num} is: {result}")
-
-
